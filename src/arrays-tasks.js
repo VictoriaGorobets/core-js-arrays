e33614bd7a7d381fe9c61265f6fe1d5db28769ae
VictoriaGorobets <vickyhome1996@gmail.com>
2024-01-02 17:17:52 +0300

    fix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -22,7 +22,7 @@
  */
 function getIntervalArray(start, end) {
   const result = [];
-  for (let i = start; i <= end; i++) {
+  for (let i = start; i <= end; i+=) {
     result.push(i);
   }
   return result;
